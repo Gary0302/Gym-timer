4dd25600ed7753f085ad216160ddb5ce61fe7daf
Gary <yanggary2388@gmail.com>
2026-02-06 17:55:24 -0800

    Initial Commit

---
+++ Gym-timer/Gym_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_timerApp.swift
+//  Gym-timer
+//
+//  Created by Gary yang on 2026/2/6.
+//
+
+import SwiftUI
+
+@main
+struct Gym_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
